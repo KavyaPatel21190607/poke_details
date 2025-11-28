7509e01f2ef1a447b6fe73195468155b5d8d45ef
Kavya Patel <webdevloper07070@gmail.com>
2025-11-28 11:02:43 +0530

    Update pokemon-list.tsx

--- src/components/pokemon-list.tsx
+++ src/components/pokemon-list.tsx
@@ -29,7 +29,7 @@ export function PokemonList({ onSelectPokemon, apiKey }: PokemonListProps) {
     setLoading(true);
     try {
       const offset = (currentPage - 1) * itemsPerPage;
-      const url = `https://pokeapi.co/api/v2/pokemon?limit=10000&offset=${offset}`;
+      const url = `https://pokeapi.co/api/v2/pokemon?limit=${itemsPerPage}&offset=${offset}`;
       
       const response = await fetch(url);
       const data = await response.json();
